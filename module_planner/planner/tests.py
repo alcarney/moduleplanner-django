62858b673c44363bbb7c6532f841234e851699cb
Alex Carney <alex.rugby101@gmail.com>
2014-10-31 07:22:17 +0000

    Trying to get testing to work

--- module_planner/planner/tests.py
+++ module_planner/planner/tests.py
@@ -1,3 +1,6 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
 
-# Create your tests here.
+class simple_tests(SimpleTestCase):
+
+    def test_math(self):
+        self.assertEqual(1 + 1, 3)
